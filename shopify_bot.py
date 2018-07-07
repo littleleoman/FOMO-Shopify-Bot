f80e0a95e0f50f11a6aadbc8d26caa62031a0cb4
Leo Oliveira <Jesus@Leos-MacBook-Air.local>
2018-07-06 21:01:11 -0400

    Initial commit

---
+++ shopify_bot.py
@@ -0,0 +1,135 @@
+'''
+@author: yung_messiah
+
+'''
+import requests
+import bs4
+import re 
+# import random
+# import webbrowser
+
+''' Retrieves sizes for item in stock.
+
+    @param url: The url passed by the user pointing to the item he/she wants ''' 
+def get_sizes(url):
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+    #url = "https://kith.com/collections/footwear/products/nkaq0996-001"
+    
+    # Ensure url starts with https:// in case url only contains www....
+    url_formatting = re.match('https://', url)
+    if url_formatting == None:
+        url = 'https://' + url
+    
+    raw_HTML = requests.get(url, headers=headers, timeout=5)
+    if raw_HTML.status_code != 200:
+        print("An error has occured completing your request")
+        return False
+    else:
+        page = bs4.BeautifulSoup(raw_HTML.text, 'lxml')
+    
+        print(page.title.string)
+        get_size_variant(url, page)
+        
+''' Retrieves only the absolute URL from passed in URL.
+
+    @param url: The address passed in by the user '''
+def get_absolute_url(url):
+    absolute_url = re.match('https://', url)
+    if absolute_url == None:
+        absolute_url = re.match('[a-zA-Z0-9.-]+/', url)
+        if absolute_url == None:
+            return False
+        absolute_url = absolute_url.group()
+        return absolute_url
+    else:
+        absolute_url = re.match('https://[a-zA-Z0-9.-]+/', url)
+        absolute_url = absolute_url.group()
+        return absolute_url
+    
+    
+''' Retrieves the id associated to the item size (required to create a link). 
+
+    @param url: The item's url  
+    @param page: Page information retrieved through requests '''
+def get_size_variant(url, page):
+    scripts = page.find_all("script")
+    if scripts == None:
+        print("An error has occured completing your request")
+        return False
+    
+    script_index = find_variant_script(scripts)
+    
+    script = scripts[script_index].getText()
+    
+    ''' split it in this manner to store items of script separated by a new line '''
+    script = script.split(';')
+    ''' retrieve only the line containing size information '''
+    script = script[3]
+    ''' split in this manner so that each size is a different list item '''
+    script = script.split('{\"id\":')
+    ''' remove unwanted information in beginning of list '''
+    script.remove(script[0])
+    script.remove(script[0])
+      
+    for item in script:
+        if 'public_title\":\"' in item:
+            data = item
+            data = data.split(',')
+            
+            size = data[3].split("\"")
+            size = size[3]
+            print(size)
+            retrieved_id = data[0]
+            
+            # add leading and trailing spaces to make regex matching easier
+            size = " " + size + " "
+            item_size = re.search('\s\d+\.\d+\s', str(size))
+            if item_size == None:
+                item_size = re.search('\s\d{1,2}\s', str(size))
+                if item_size == None:
+                    item_size = re.search('(?i)(XS|X-S|(\sS\s|Small)|(\sM\s|Medium)|(\sL\s|Large)' + 
+                                                  '|XL|XXL|XXXL|X-L|XX-L|XXX-L)', str(size))
+                    if item_size == None:
+                        item_size = size
+            
+            if item_size != size:
+                item_size = item_size.group()
+                
+            item_size = item_size.replace('\\', '')
+            item_size = item_size.replace('/', "")
+            print_link(url, item_size, str(retrieved_id))
+            
+    return True
+
+
+''' Prints a correctly formated link which takes user straight to purchase.
+
+    @param url: URL of the item to be bought
+    @param size: Size of the given item
+    @param retrieved_id: Id associated to the item size  '''
+def print_link(url, size, retrieved_id):
+    absolute_url = get_absolute_url(url)
+    if absolute_url == False:
+        print("An error has occured completing your request")
+        return False
+
+    print('[ ' + size +  ' ] \t',absolute_url + 'cart/' + retrieved_id + ':1')
+
+''' Kickstarts the entire script.
+
+    @param url: The url pointing to the item which the user wants to buy '''
+def run(url):
+    get_sizes(url)
+    
+def find_variant_script(scripts):  
+    ''' Loops through all the scripts on page to find correct script containing size data 
+            We uncomment the code below if we get errors in the future retrieving data from 
+            the page
+    '''
+    for number, script in enumerate(scripts):
+        if "variants\":[{" in script.getText():
+            return number
+            break
+            
+
+# run("https://rockcitykicks.com/products/born-x-raised-snooty-fox-t-shirt-white")
